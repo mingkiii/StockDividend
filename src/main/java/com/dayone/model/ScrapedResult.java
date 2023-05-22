11ed7cd01921ad7d600cc11e91add2cf836d51ba
mingkiii <kimchunjae92@gmail.com>
2023-05-22 16:39:21 +0900

    File commit

---
+++ src/main/java/com/dayone/model/ScrapedResult.java
@@ -0,0 +1,22 @@
+package com.dayone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ScrapedResult {
+
+    private Company company;
+
+    private List<Dividend> dividends;
+
+    public ScrapedResult() {
+        this.dividends = new ArrayList<>();
+    }
+}
---
+++ src/test/java/dayone/dayone/StockDividendApplicationTests.java
@@ -0,0 +1,13 @@
+package dayone.dayone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDividendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
